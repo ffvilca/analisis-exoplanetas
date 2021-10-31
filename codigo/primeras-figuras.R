3d60a79b48847047b80009dd0140e9a99de4b7c7
Fran Vilca <fvilca@uc.cl>
2021-10-30 21:54:05 -0300

    Crear nuevos gráficos y aprender mayor más sobre el paquete ggplot

--- codigo/primeras-figuras.R
+++ codigo/primeras-figuras.R
@@ -66,17 +66,85 @@ ggsave("figuras/columnas_pib-chile.png", height = 7, width = 10)
 desarrollo |> 
   filter(country == "Chile") |> 
   ggplot(aes(x = year, y = gdpPercap)) +
-  geom_line() +
-  geom_point() +
+  geom_line(color = "turquoise3") +
+  geom_point(color = "turquoise4") +
   theme_minimal() +
   labs(title = "Evolución del PIB en Chile (1952 - 2007)",
        y = "PIB percapita en dolares",
        x = NULL)+
   scale_y_continuous(labels = scales::dollar)+
-  geom_text(aes(label = round(gdpPercap)), vjust = -0.8, hjust = 1)+
+  geom_text(aes(label = round(gdpPercap)), vjust = -0.8, hjust = 1, color = "turquoise4")+
   scale_x_continuous(limits = c(1945,2010), breaks = seq(1945,2010,10))
 
 # geom_text sirve para poner etiquetas
 
 ggsave("figuras/linea_pib-chile.png", height = 7, width = 10)
+
+
+# EJERCICIO 3: gráfico de barras ------------------------------------------
+desarrollo |> 
+  filter(country %in% c("Chile", "Argentina")) |> 
+  ggplot(aes(year,gdpPercap, fill= country))+
+  geom_col(position = "dodge") + 
+  theme_minimal()+
+  scale_fill_manual(values = c("#FF4D00","turquoise3"))+
+  labs(x = NULL,
+       y = "PIB per capita en dolares",
+       title = "Evolución del PIB per capita en Chile y Argentina",
+       subtitle = "1952 a 2007",
+       fill = "pais")
+
+# dodge -> poner a los lados
+# EJERCICIO 4 : anotar y destacar ----------------------------------------
+
+desarrollo |> 
+  filter(region == "South America") |>
+  ggplot(aes(year, gdpPercap, color=country))+
+  geom_line()+
+  theme_minimal()+
+  labs(x = NULL,
+       y = "PIB per capita en dolares",
+       title = "Evolución del PIB en Sud América",
+       subtitle = "años 1952 a 2007") +
+  scale_y_continuous(limits = c(0,15000), labels = scales::dollar) +
+  geom_vline(xintercept = 1997, color = "gray" , linetype = "dashed")+
+  geom_label(aes(x = 1989, y = 14000, label = "1997: Crisis Asiatica"),
+             color = "gray51", size =3)
+
+# versión 2 enfoque de solo 1 país
+
+desarrollo |> 
+  filter(region == "South America") |>
+  ggplot(aes(year, gdpPercap, color=country))+
+  geom_line()+
+  gghighlight(country == "Chile")+
+  scale_color_manual(values = "turquoise3")+
+  theme_minimal()+
+  labs(x = NULL,
+       y = "PIB per capita en dolares",
+       title = "Evolución del PIB en Chile entre 1952 y 2007",
+       subtitle = "Comparación con otros países de Sudamerica") +
+  scale_y_continuous(limits = c(0,15000), labels = scales::dollar) +
+  geom_vline(xintercept = 1997, color = "gray" , linetype = "dashed")+
+  geom_label(aes(x = 1989, y = 14000, label = "1997: Crisis Asiatica"),
+             color = "gray51", size =3)
+
+
+
+# EJERCICIO 5 : otra forma de construir una leyenda para un gráfico -------
+
+  desarrollo |> 
+  filter(country %in% c("Chile", "Argentina")) |> 
+  ggplot(aes(year,gdpPercap, fill= country))+
+  geom_col(position = "dodge", show.legend = F) + 
+  theme_minimal()+
+  scale_fill_manual(values = c("#e35d6a","#005B96"))+
+  labs(x = NULL,
+       y = "PIB per capita en dolares",
+       title = "Evolución del PIB per capita en <b style='color:#005B96'> Chile </b> y <b style='color:#e35d6a'> Argentina </b>",
+       subtitle = "1952 a 2007",
+       fill = "pais")+
+  theme(plot.title = element_markdown())
+
+# dodge -> poner a los lados
 
